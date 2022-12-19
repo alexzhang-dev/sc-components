8c8e7d7d3435ba94a706a8e92527311f2b22e008
alexzhang1030 <1642114555@qq.com>
2022-12-19 11:04:34 +0800

    chore: setup unit test

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+import solid from 'vite-plugin-solid'
+
+export default defineConfig({
+  plugins: [solid()],
+  test: {
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
+    environment: 'happy-dom',
+  },
+})
